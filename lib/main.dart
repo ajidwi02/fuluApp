5e42d9bf952b102ddbe2d846f6fb24935ca52481
ajidwi02 <ajidwi111202@gmail.com>
2024-08-01 22:23:43 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+
+import 'action_button.dart';
+import 'credit_card.dart';
+import 'transaction_list.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fintech App',
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromARGB(255, 16, 80, 90)),
+          useMaterial3: true),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 16, 80, 90),
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Welcome Back",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      Text(
+                        "Aji Dwi Prakoso",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                  const Spacer(),
+                  IconButton.outlined(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.notifications,
+                        color: Colors.white,
+                      ))
+                ],
+              ),
+            ),
+            Expanded(
+              child: Stack(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(top: 167),
+                    color: Colors.white,
+                    child: const Column(
+                      children: [
+                        SizedBox(
+                          height: 110,
+                        ),
+                        ActionButtons(),
+                        SizedBox(height: 30),
+                        TransactionList()
+                      ],
+                    ),
+                  ),
+                  const Positioned(
+                    top: 20,
+                    left: 25,
+                    right: 25,
+                    child: CreditCard(),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: const Column(
+                children: [
+                  Icon(Icons.home),
+                  Text(
+                    "Home",
+                    style: TextStyle(fontSize: 10),
+                  )
+                ],
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Column(
+                children: [
+                  Icon(Icons.credit_card),
+                  Text(
+                    "My Card",
+                    style: TextStyle(fontSize: 10),
+                  )
+                ],
+              ),
+            ),
+            FloatingActionButton(
+              onPressed: () {},
+              backgroundColor: const Color.fromARGB(255, 16, 80, 90),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: const Icon(
+                Icons.qr_code_scanner,
+                color: Colors.white,
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Column(
+                children: [
+                  Icon(Icons.bar_chart),
+                  Text(
+                    "Activity",
+                    style: TextStyle(fontSize: 10),
+                  )
+                ],
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Column(
+                children: [
+                  Icon(Icons.person),
+                  Text(
+                    "Profile",
+                    style: TextStyle(fontSize: 10),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/transaction_list.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class TransactionList extends StatelessWidget {
+  const TransactionList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView(
+        children: [
+          const Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Today, Aug 1"),
+                Row(
+                  children: [Text("Transactions")],
+                ),
+              ],
+            ),
+          ),
+          Divider(color: Colors.grey[200]),
+          const ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Color.fromARGB(255, 239, 243, 245),
+              child: Icon(
+                Icons.fitness_center,
+                color: Colors.purpleAccent,
+              ),
+            ),
+            title: Text("Gym"),
+            subtitle: Text("Payment"),
+            trailing: Text(
+              "-\Rp 150.00",
+              // style: TextStyle(color: Colors.red),
+            ),
+          ),
+          Divider(color: Colors.grey[200]),
+          const ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Color.fromARGB(255, 239, 243, 245),
+              child: Icon(
+                Icons.account_balance,
+                color: Colors.teal,
+              ),
+            ),
+            title: Text("Bank BNI 46"),
+            subtitle: Text("Deposit"),
+            trailing: Text(
+              "+\Rp 15.000.000",
+              style: TextStyle(color: Colors.teal),
+            ),
+          ),
+          Divider(color: Colors.grey[200]),
+          const ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Color.fromARGB(255, 239, 243, 245),
+              child: Icon(
+                Icons.send,
+                color: Colors.orangeAccent,
+              ),
+            ),
+            title: Text("BKK Polines"),
+            subtitle: Text("Send"),
+            trailing: Text(
+              "-\Rp 4.500.000",
+              // style: TextStyle(color: Colors.red),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
